0749a0bf93306a3ef7be0bf25c5ac5a8971c0c18
tejaas07 <himanshujhar@gmail.com>
2025-10-31 10:15:31 +0100

    add files that were setup outside modules

--- homework_assignment_1_group_2_oops_owlz/TaskStatus.java
+++
@@ -1,8 +0,0 @@
-package application.modules;
-
-public enum TaskStatus {
-    PENDING,
-    IN_PROGRESS,
-    COMPLETED,
-    CANCELLED
-}
--- homework_assignment_1_group_2_oops_owlz/TaskType.java
+++
@@ -1,6 +0,0 @@
-package application.modules;
-
-public enum TaskType {
-    PICKUP,
-    DELIVERY
-}
